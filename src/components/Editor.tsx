73dae03bc5bcbfbc316b03b182bf1a46f03185d5
Dakota Madden-Fong <maddenfong@gmail.com>
2021-12-31 12:22:00 -0800

    began working on button functionality

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 import {
     Grid,
@@ -9,9 +9,13 @@ import {
 } from '@material-ui/core';
 import {
     FormatBold as BoldIcon,
+    FormatItalic as ItalicIcon,
 } from '@material-ui/icons';
 
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, {
+    ReactCodeMirrorRef,
+    TransactionSpec,
+} from '@uiw/react-codemirror';
 import {
     markdown,
     markdownLanguage,
@@ -50,24 +54,57 @@ const Editor = (props: {}): JSX.Element => {
     const classes = useStyles();
     const dispatch = useAppDispatch();
 
+    // current global dark mode state
     const darkMode = useAppSelector(selectDarkMode);
+
+    // ref for codemirror component
+    const codeMirrorRef = useRef<ReactCodeMirrorRef>(null);
 
     // handler for updating state when text is changed
     const inputMD = useAppSelector(selectInputMD);
     const handleChange = (value: string) => dispatch(updateText(value));
 
+    const insertBold = () => {
+
+        if (!codeMirrorRef.current) return;
+        const {view} = codeMirrorRef.current;
+        if (!view) return;
+
+        const transactionSpecs = view.state.selection.ranges.map((range) => view.state.update(
+            {changes: {
+                from: range.from,
+                insert: '**',
+            }},
+            {changes: {
+                from: range.to,
+                insert: '**',
+            }},
+        ));
+
+        const transaction = view.state.update(...transactionSpecs);
+        view.dispatch(transaction);
+
+        codeMirrorRef.current.editor?.focus();
+
+    };
+
     return (
         <Box className={classes.root}>
             <Grid container>
+
+                {/* Toolbar With helper buttons */}
                 <Grid item xs={12}>
                     <Toolbar variant='dense'>
-                        <IconButton
-                            size='small'
-                        >
+                        <IconButton size='small' onClick={insertBold}>
                             <BoldIcon />
+                        </IconButton>
+                        <IconButton size='small'>
+                            <ItalicIcon />
                         </IconButton>
                     </Toolbar>
                 </Grid>
+
+                {/* CodeMirror */}
                 <Grid item xs={12}>
                     <CodeMirror
                         theme={darkMode ? 'dark' : 'light'}
@@ -80,6 +117,7 @@ const Editor = (props: {}): JSX.Element => {
                             }),
                         ]}
                         className={classes.codeMirror}
+                        ref={codeMirrorRef}
                     />
                 </Grid>
             </Grid>
