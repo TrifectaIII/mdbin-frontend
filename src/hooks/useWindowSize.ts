c495d1a17b918b0dbe25f0554fb5784816e87e46
Dakota Madden-Fong <maddenfong@gmail.com>
2022-01-04 18:27:25 -0800

    added comments

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -3,18 +3,22 @@ import {
     useState,
 } from 'react';
 
+// type of return object
 export interface WindowSize {
     width: number;
     height: number;
 }
 
+// uses state and effect hooks to track window size
 const useWindowSize = (): WindowSize => {
 
+    // state to track size
     const [windowSize, setWindowSize] = useState<WindowSize>({
         width: 0,
         height: 0,
     });
 
+    // handler pulls from global window object
     const handleResize = (): void => setWindowSize({
         width: window.innerWidth,
         height: window.innerHeight,
@@ -22,11 +26,17 @@ const useWindowSize = (): WindowSize => {
 
     useEffect(() => {
 
+        // set up a listener
         window.addEventListener('resize', handleResize);
+
+        // execute the function once to start
         handleResize();
+
+        // return a cleanup function to remove the listener
         return () => window.removeEventListener('resize', handleResize);
 
     }, []);
+    // empty dep array means this will only run once
 
     return windowSize;
 
