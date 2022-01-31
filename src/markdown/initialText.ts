407bbbce1b9ed16757b9474339ff5dfc05c59193
Dakota Madden-Fong <maddenfong@gmail.com>
2022-01-30 17:06:45 -0800

    new starting text

--- src/markdown/initialText.ts
+++ src/markdown/initialText.ts
@@ -13,4 +13,51 @@ Brush up with the [cheat sheet](https://www.markdownguide.org/cheat-sheet/).
 All the basic syntax and some of the extended syntax will work. Try things out!
 
 When you're ready to share, just publish your document.
+
+# H1
+## H2
+### H3
+
+**bold text**
+
+*italicized text*
+
+> blockquote
+> 
+> can be many lines
+
+1. First item
+2. Second item
+3. Third item
+
+- First item
+- Second item
+- Third item
+
+\`code inline\`
+
+\`\`\`
+"code block" : {
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+
+| Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text |
+
+Here's a sentence with a footnote. [^1]
+
+[^1]: This is the footnote.
+
+~~The world is flat.~~
+
+- [x] Write the press release
+- [ ] Update the website
+- [ ] Contact the media
+
+
 `;
