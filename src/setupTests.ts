093ee89fd81c4ebb1a22c2b805be546f5a6f18fe
Dakota Madden-Fong <maddenfong@gmail.com>
2022-02-07 17:13:56 -0800

    added links to testing resources

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// https://reactjs.org/docs/testing.html
+
+// https://redux.js.org/usage/writing-tests
