43fc11cd020ee0375031483bd7e6019885e73eae
Dakota Madden-Fong <maddenfong@gmail.com>
2022-02-14 13:48:20 -0800

    added first test

--- src/features/view/viewSlice.spec.ts
+++ src/features/view/viewSlice.spec.ts
@@ -1,4 +1,21 @@
-import ViewReducer, {
+import viewReducer, {
     ViewState,
+    resetView,
 } from './viewSlice';
 
+test('resetView should reset all fields', () => {
+
+    const previousState: ViewState = {
+        requestStatus: 'success',
+        documentKey: '123',
+        text: 'abc',
+        published: 1,
+    };
+    expect(viewReducer(previousState, resetView())).toEqual({
+        requestStatus: 'idle',
+        documentKey: null,
+        text: null,
+        published: null,
+    });
+
+});
